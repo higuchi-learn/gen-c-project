acd7756704a138c0a3fb0e0d257244becd62c1e9
higuchi-learn <haruki.higuchi.000@gmail.com>
2026-03-25 03:01:33 +0900

    init

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+// @ts-check
+import { defineConfig } from 'eslint/config';
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tslint from 'typescript-eslint';
+
+export default defineConfig(
+  {
+    ignores: ['eslint.config.mjs', 'src/api.types.gen.ts', 'src/routeTree.gen.ts', 'dist/**'],
+  },
+  eslint.configs.recommended,
+  ...tslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/only-throw-error': 'off',
+      '@typescript-eslint/no-base-to-string': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': ['error', { endOfLine: 'lf' }],
+    },
+  },
+);
