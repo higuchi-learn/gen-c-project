52c93c2bc290f2232dd9d82a0ef2696208393991
higuchi-learn <haruki.higuchi.000@gmail.com>
2026-03-27 01:32:51 +0900

    add: conectingtest

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 export const supabase = createClient(
-  process.env.VITE_SUPABASE_URL!,
-  process.env.VITE_SUPABASE_ANON_KEY!
+  import.meta.env.VITE_SUPABASE_URL as string,
+  import.meta.env.VITE_SUPABASE_ANON_KEY as string,
 );
